0104fae0b2889b25851800ef243cd3fafb92ce10
gordonlzy <gordon.ampotech@gmail.com>
2024-06-26 16:50:03 +0800

    Set up ingress

---
+++ vida-google-assistant/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Start Nginx with specified options
+nginx -g 'daemon off; error_log /dev/stdout debug;' &
+
+# Start the Flask app in the background
+gunicorn -b 0.0.0.0:8099 app:app &
+
+wait
